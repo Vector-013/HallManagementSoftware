12dd24b37eb15700a1d5a7354a518faa7d5c11ee
Vector-013 <vectorprime1397@gmail.com>
2024-03-12 21:21:38 +0530

    try again

--- hms/app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class User(models.Model):
-    name = models.CharField(max_length=8, default="")
-    score = models.IntegerField(null=False, default=666)
-    alive = models.BooleanField(null=False, default=False)
