a407f8e83fba7056a74c48a3b28cb7de11b1f19e
ShreyaBose04 <shreya.bose.in@gmail.com>
2024-03-12 16:03:36 +0530

    Added Django App

---
+++ hms/app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.http import HttpResponse
+
+# Create your models here.
+
+
+def main(request):
+    return HttpResponse("Hello")
---
+++ hms/app/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from .views import main
+
+urlpatterns = [path("", main)]
