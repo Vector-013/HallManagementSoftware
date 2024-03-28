b1f71b7ee54028094ddbff0dd9feaf23e6fc5e1b
Vector-013 <vectorprime1397@gmail.com>
2024-03-28 10:45:18 +0530

    lo

---
+++ hms/main/custom_views/hmc_views.py
@@ -0,0 +1,220 @@
+from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from ..forms import *
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
+from ..models import *
+import uuid
+
+
+def register_warden(request):
+    if request.method == "POST":
+        form = WardenRegistrationForm(request.POST)
+        if form.is_valid():
+            print(request.POST)
+            stakeholderID = form.cleaned_data.get("stakeholderID")
+            email = form.cleaned_data.get("email")
+            address = form.cleaned_data.get("address")
+            mobile = form.cleaned_data.get("mobile")
+            first_name = form.cleaned_data.get("first_name")
+            last_name = form.cleaned_data.get("last_name")
+            password = form.cleaned_data.get("password")
+            hall = form.cleaned_data.get("hall")
+            token = str(uuid.uuid4())
+            department = form.cleaned_data.get("department")
+            designation = form.cleaned_data.get("designation")
+            posts_held = form.cleaned_data.get("posts_held")
+            client = Client.objects.create_user(
+                stakeholderID,
+                email,
+                password,
+                mobile,
+                first_name,
+                last_name,
+                address,
+                token,
+                "warden",
+            )
+            warden = Warden(
+                client=client,
+                hall=hall,
+                department=department,
+                designation=designation,
+                posts_held=posts_held,
+            )
+            warden.save()
+            subject = "Your account needs to be verified"
+            message = f"Hi, click on this link to verify your account http://127.0.0.1:8000/hmc/verify-warden/{token}"
+            email_from = "se.mhc.2024@gmail.com"
+            recipient_list = [email]
+            send_mail(subject, message, email_from, recipient_list)
+
+            messages.success(
+                request,
+                f"Pls click on the link sent to {email} to complete registration",
+            )
+            return redirect("/login")
+    else:
+        form = WardenRegistrationForm()
+    return render(
+        request,
+        "hmc/register_warden.html",
+        context={"form": form, "title": "register"},
+    )
+
+
+def verify_warden(request, token):
+    client = Client.objects.filter(token=token).first()
+    if client:
+        client.is_active = True
+        client.save()
+        messages.info(request, "Your account has been verified")
+        return redirect("/login")
+    else:
+        return redirect("/error")
+
+
+def hmc_landing(request):
+    return render(request, "hmc/landing.html")
+
+
+def register_hall(request):
+    if request.method == "POST":
+        form = HallRegistrationForm(request.POST)
+
+        if form.is_valid():
+
+            name = form.cleaned_data.get("name")
+            no_of_blocks = form.cleaned_data.get("blocks")
+            no_of_floors = form.cleaned_data.get("floors")
+            singles = form.cleaned_data.get("singles")
+            rent_singles = form.cleaned_data.get("rent_singles")
+            doubles = form.cleaned_data.get("doubles")
+            rent_doubles = form.cleaned_data.get("rent_doubles")
+            triples = form.cleaned_data.get("triples")
+            rent_triples = form.cleaned_data.get("triples")
+
+            print(int(rent_singles))
+
+            hall = Hall.objects.create(
+                name=name,
+                singles=singles,
+                doubles=doubles,
+                triples=triples,
+                floors=no_of_floors,
+                blocks=no_of_blocks,
+            )
+            hall.save()
+
+            for x in range(int(no_of_blocks)):
+                for y in range(int(no_of_floors)):
+                    for z in range(int(singles)):
+                        room = Room(
+                            hall=hall,
+                            rent=rent_singles,
+                            sharing=1,
+                            floor=y,
+                            block=chr(65 + x),
+                            number=z,
+                            code=name + "-" + str(chr(65 + x)) + str(y) + str(z),
+                        )
+                        room.save()
+
+            x = 0
+            y = 0
+
+            for x in range(int(no_of_blocks)):
+                for y in range(int(no_of_floors)):
+                    for z in range(int(singles), int(singles) + int(doubles)):
+                        room = Room(
+                            hall=hall,
+                            rent=rent_doubles,
+                            sharing=2,
+                            floor=y,
+                            block=chr(65 + x),
+                            number=z,
+                            code=name + "-" + str(chr(65 + x)) + str(y) + str(z),
+                        )
+                        room.save()
+
+            x = 0
+            y = 0
+
+            for x in range(int(no_of_blocks)):
+                for y in range(int(no_of_floors)):
+                    for z in range(
+                        int(singles) + int(doubles),
+                        int(singles) + int(doubles) + int(triples),
+                    ):
+                        room = Room(
+                            hall=hall,
+                            rent=rent_triples,
+                            sharing=3,
+                            floor=y,
+                            block=chr(65 + x),
+                            number=z,
+                            code=name + "-" + str(chr(65 + x)) + str(y) + str(z),
+                        )
+                        room.save()
+
+            hall.max_occupancy = hall.calculate_max_occupancy()
+            hall.save()
+            hall_passbook = HallPassbook.objects.create(hall=hall)
+            mess_passbook = MessPassbook.objects.create(hall=hall)
+            warden_passbook = WardenPassbook.objects.create(hall=hall)
+            messages.info(request, f"Hall {name} has been created")
+            return redirect("/hmc/landing")
+
+    else:
+        form = HallRegistrationForm()
+
+    return render(request, "hmc/hall_registration.html", {"form": form})
+
+
+def hmc_landing(request):
+    return render(request, "hmc/landing.html")
+
+
+def grant_allotment(request):
+    if request.method == "POST":
+        form = GrantForm(request.POST)
+        if form.is_valid():
+            password_to_confirm = form.cleaned_data.get("verify_password")
+            hall = form.cleaned_data.get("hall")
+            amount = form.cleaned_data.get("amount")
+            client = request.user
+            success = client.check_password(password_to_confirm)
+            if success:
+                # try:
+                warden_passbook = WardenPassbook.objects.filter(hall=hall)
+
+                warden_transaction = WardenTransaction(
+                    type="grant",
+                    timestamp=datetime.now(),
+                    amount=amount,
+                    warden_passbook=warden_passbook,
+                )
+                warden_transaction.save()
+                # except:
+                messages.success(request, "Your grant has been alloted old codger")
+                redirect("/hmc/landing")
+
+            # except:
+            #     messages.error(request, "Connection issues")
+            else:
+                messages.error(request, "you are unreal")
+                redirect("/login")
+    else:
+        form = GrantForm()
+
+    return render(
+        request,
+        "hmc/verify_password.html",
+        context={"form": form, "title": "verify"},
+    )
---
+++ hms/main/custom_views/warden_views.py
@@ -0,0 +1,386 @@
+from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from ..forms import *
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
+from ..models import *
+import uuid
+from django.http import HttpResponse
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
+
+def hall_manager(request):
+    warden = Warden.objects.filter(client=request.user).first()
+    hall = warden.hall
+    hall_manager = HallManager.objects.filter(hall=hall).first()
+    if hall_manager:
+        return redirect("/hall/passbook")
+    else:
+        return redirect("/warden/register-hall-manager")
+
+
+def mess_manager(request):
+    warden = Warden.objects.filter(client=request.user).first()
+    hall = warden.hall
+    mess_manager = MessManager.objects.filter(hall=hall).first()
+    if mess_manager:
+        return redirect("/mess/passbook")
+    else:
+        return redirect("/warden/register-mess-manager")
+
+
+def register_hall_manager(request):
+    if request.method == "POST":
+        form = ManagerRegistrationForm(request.POST)
+        warden = Warden.objects.filter(client=request.user).first()
+        if form.is_valid():
+            print(request.POST)
+            stakeholderID = form.cleaned_data.get("stakeholderID")
+            email = form.cleaned_data.get("email")
+            address = form.cleaned_data.get("address")
+            mobile = form.cleaned_data.get("mobile")
+            first_name = form.cleaned_data.get("first_name")
+            last_name = form.cleaned_data.get("last_name")
+            password = form.cleaned_data.get("password")
+            hall = warden.hall
+            token = str(uuid.uuid4())
+            client = Client.objects.create_user(
+                stakeholderID,
+                email,
+                password,
+                mobile,
+                first_name,
+                last_name,
+                address,
+                token,
+                "hall_manager",
+            )
+            hall_manager = HallManager(client=client, hall=hall)
+            hall_manager.save()
+            subject = "Your account needs to be verified"
+            message = f"Hi, click on this link to verify your account http://127.0.0.1:8000/warden/verify-manager/{token}"
+            email_from = "shreya.bose.in@gmail.com"
+            recipient_list = [email]
+            send_mail(subject, message, email_from, recipient_list)
+
+            messages.success(
+                request,
+                f"Pls click on the link sent to {email} to complete registration",
+            )
+            return redirect("/warden/landing")
+    else:
+        form = ManagerRegistrationForm()
+    return render(
+        request,
+        "warden/register_manager.html",
+        context={"form": form, "title": "register"},
+    )
+
+
+def register_mess_manager(request):
+    if request.method == "POST":
+        form = ManagerRegistrationForm(request.POST)
+        warden = Warden.objects.filter(client=request.user).first()
+        if form.is_valid():
+            print(request.POST)
+            stakeholderID = form.cleaned_data.get("stakeholderID")
+            email = form.cleaned_data.get("email")
+            address = form.cleaned_data.get("address")
+            mobile = form.cleaned_data.get("mobile")
+            first_name = form.cleaned_data.get("first_name")
+            last_name = form.cleaned_data.get("last_name")
+            password = form.cleaned_data.get("password")
+            hall = warden.hall
+            token = str(uuid.uuid4())
+            client = Client.objects.create_user(
+                stakeholderID,
+                email,
+                password,
+                mobile,
+                first_name,
+                last_name,
+                address,
+                token,
+                "mess_manager",
+            )
+            mess_manager = MessManager(client=client, hall=hall)
+            mess_manager.save()
+            subject = "Your account needs to be verified"
+            message = f"Hi, click on this link to verify your account http://127.0.0.1:8000/warden/verify-manager/{token}"
+            email_from = "shreya.bose.in@gmail.com"
+            recipient_list = [email]
+            send_mail(subject, message, email_from, recipient_list)
+
+            messages.success(
+                request,
+                f"Pls click on the link sent to {email} to complete registration",
+            )
+            return redirect("/warden/landing")
+    else:
+        form = ManagerRegistrationForm()
+    return render(
+        request,
+        "warden/register_manager.html",
+        context={"form": form, "title": "register"},
+    )
+
+
+def verify_manager(request, token):
+    client = Client.objects.filter(token=token).first()
+    if client:
+        client.is_active = True
+        client.save()
+        messages.info(request, "Your account has been verified")
+        return redirect("/warden/landing")
+    else:
+        return redirect("/error")
+
+
+def generate_hall_demand(request):
+    if request.method == "POST":
+        form = VerifyForm(request.POST)
+        if form.is_valid():
+            password_to_confirm = form.cleaned_data.get("verify_password")
+            amount_per_student = form.cleaned_data.get("amount")
+            client = request.user
+            success = client.check_password(password_to_confirm)
+            if success:
+                try:
+                    warden = Warden.objects.filter(client=client).first()
+                    hall = warden.hall
+                    students = list(hall.student_hall.all())
+
+                    for student in students:
+                        student_passbook = StudentPassbook.objects.filter(
+                            student=student
+                        ).first()
+                        due = Due(
+                            type="hall",
+                            timestamp=datetime.now(),
+                            demand=amount_per_student,
+                            student_passbook=student_passbook,
+                        )
+                        due.save()
+
+                    messages.success(
+                        request, "Your password has been verified old codger"
+                    )
+                    redirect("/warden/landing")
+
+                except:
+                    messages.error(request, "Connection issues")
+            else:
+                messages.error(request, "you are unreal")
+                redirect("/login")
+    else:
+        form = VerifyForm()
+
+    return render(
+        request,
+        "warden/verify_password.html",
+        context={"form": form, "title": "verify"},
+    )
+
+
+def generate_salary(request):
+    if request.method == "POST":
+        form = ConfirmForm(request.POST)
+        if form.is_valid():
+            password_to_confirm = form.cleaned_data.get("verify_password")
+            client = request.user
+            success = client.check_password(password_to_confirm)
+            if success:
+                # try:
+                warden = Warden.objects.filter(client=client).first()
+                hall = warden.hall
+                employees = list(hall.employee_hall.all())
+                total = 0
+                hall_passbook = HallPassbook.objects.filter(hall=hall).first()
+                for employee in employees:
+                    total = total + employee.salary
+
+                transaction = HallTransaction(
+                    type="salaries",
+                    timestamp=datetime.now(),
+                    amount=total,
+                    hall_passbook=hall_passbook,
+                )
+                transaction.save()
+                messages.success(request, "Hall employees have been paid old codger")
+                redirect("/warden/landing")
+
+            # except:
+            #     messages.error(request, "Connection issues")
+            else:
+                messages.error(request, "you are unreal")
+                redirect("/login")
+    else:
+        form = ConfirmForm()
+
+    return render(
+        request,
+        "warden/verify_password.html",
+        context={"form": form, "title": "verify"},
+    )
+
+
+def generate_mess_demand(request):
+    if request.method == "POST":
+        form = VerifyForm(request.POST)
+        if form.is_valid():
+            password_to_confirm = form.cleaned_data.get("verify_password")
+            amount_per_student = form.cleaned_data.get("amount")
+            client = request.user
+            success = client.check_password(password_to_confirm)
+            if success:
+                # try:
+                warden = Warden.objects.filter(client=client).first()
+                hall = warden.hall
+                students = list(hall.student_hall.all())
+
+                for student in students:
+                    student_passbook = StudentPassbook.objects.filter(
+                        student=student
+                    ).first()
+                    due = Due(
+                        type="mess",
+                        timestamp=datetime.now(),
+                        demand=amount_per_student,
+                        student_passbook=student_passbook,
+                    )
+                    due.save()
+                # except:
+                messages.success(request, "Your password has been verified old codger")
+                redirect("/warden/landing")
+
+            # except:
+            #     messages.error(request, "Connection issues")
+            else:
+                messages.error(request, "you are unreal")
+                redirect("/login")
+    else:
+        form = VerifyForm()
+
+    return render(
+        request,
+        "warden/verify_password.html",
+        context={"form": form, "title": "verify"},
+    )
+
+
+def warden_landing(request):
+    if request.method == "GET":
+        warden = Warden.objects.filter(client=request.user).first()
+        notices = Notice.objects.filter(hall=warden.hall)
+        print(notices)
+        return render(
+            request,
+            "warden/landing.html",
+            context={"notices": notices, "title": "Notices"},
+        )
+
+
+def allot_budget(request):
+    if request.method == "POST":
+        form = AllotmentForm(request.POST)
+        if form.is_valid():
+            password_to_confirm = form.cleaned_data.get("verify_password")
+            hall_allotment = form.cleaned_data.get("hall_allotment")
+            mess_allotment = form.cleaned_data.get("mess_allotment")
+            client = request.user
+            success = client.check_password(password_to_confirm)
+            if success:
+                # try:
+                warden = Warden.objects.filter(client=client).first()
+                hall = warden.hall
+                hall_passbook = HallPassbook.objects.filter(hall=hall).first()
+                mess_passbook = MessPassbook.objects.filter(hall=hall).first()
+                warden_passbook = WardenPassbook.objects.filter(hall=hall).first()
+                hall_transaction = HallTransaction.objects.create(
+                    type="allotment",
+                    timestamp=datetime.now(),
+                    amount=hall_allotment,
+                    hall_passbook=hall_passbook,
+                )
+                mess_transaction = MessTransaction.objects.create(
+                    type="allotment",
+                    timestamp=datetime.now(),
+                    amount=mess_allotment,
+                    mess_passbook=mess_passbook,
+                )
+                warden_hall_transaction = WardenTransaction.objects.create(
+                    type="hall_allotment",
+                    timestamp=datetime.now(),
+                    amount=hall_allotment,
+                    warden_passbook=warden_passbook,
+                )
+                warden_mess_transaction = WardenTransaction.objects.create(
+                    type="mess_allotment",
+                    timestamp=datetime.now(),
+                    amount=mess_allotment,
+                    warden_passbook=warden_passbook,
+                )
+                # except:
+                messages.success(request, "Your budgets have been alloted old codger")
+                return redirect("/warden/passbook")
+
+            # except:
+            #     messages.error(request, "Connection issues")
+            else:
+                messages.error(request, "you are unreal")
+                return redirect("/login")
+    else:
+        form = AllotmentForm()
+
+    return render(
+        request,
+        "warden/verify_password.html",
+        context={"form": form, "title": "verify"},
+    )
+
+
+def generate_warden_passbook_pdf(request):
+
+    print(request.user)
+    warden = Warden.objects.filter(client=request.user).first()
+
+    hall = hall_manager.hall
+    warden_passbook = WardenPassbook.objects.filter(hall=hall).first()
+    transactions_qset = WardenTransaction.objects.filter(
+        warden_passbook=warden_passbook
+    )
+    transactions = list(transactions_qset.all())
+
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = 'attachment; filename="Hall-Passbook.pdf"'
+
+    pdf = canvas.Canvas(response, pagesize=letter)
+
+    pdf.setFont("Helvetica", 12)
+
+    pdf.drawString(100, 750, "Type")
+    pdf.drawString(300, 750, "Amount")
+    pdf.drawString(500, 750, "Time")
+
+    y = 730
+    for transaction in transactions:
+        pdf.drawString(100, y, "{}".format(transaction.type))
+        pdf.drawString(300, y, "{}".format(transaction.amount))
+        pdf.drawString(500, y, "{}".format(transaction.timestamp))
+        y = y - 20
+
+    pdf.line(50, 700, 550, 700)
+
+    pdf.drawString(100, 400, "Signature: ___________________")
+
+    pdf.showPage()
+    pdf.save()
+
+    return response
