14af686464b2ca487afa55ccede1973efaac65e1
ShreyaBose04 <137882273+ShreyaBose04@users.noreply.github.com>
2024-05-13 17:58:37 +0530

    Delete hms directory

--- hms/main/custom_views/__init__.py
+++
@@ -1,7 +0,0 @@
-from .auth_views import *
-from .student_views import *
-from .payment_views import *
-from .hall_manager_views import *
-from .mess_manager_views import *
-from .warden_views import *
-from .hmc_views import *
